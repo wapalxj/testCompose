381a5d706ac24124c60aeb2cc142aa80985f5516
muguihai <muguihai@followme.cn>
2021-10-22 19:35:07 +0800

    fm 211022

--- app/src/main/java/com/example/compose/Main2Activity.kt
+++ app/src/main/java/com/example/compose/Main2Activity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -27,19 +28,50 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.ColorUtils
+import androidx.core.view.WindowCompat
 import com.example.compose.model.SampleData
 import com.example.compose.ui.theme.ComposeTheme
+import com.google.accompanist.insets.*
+import com.google.accompanist.insets.ui.TopAppBar
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class Main2Activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //设置沉浸式
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+//        window.statusBarColor = resources.getColor(R.color.color_t)
         setContent {
             ComposeTheme(darkTheme = false) {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.onPrimary) {
-                    TestRow()
-                }
 
+                TestStatusBarHeight()
+                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.onPrimary) {
+//                    TestRow()
+//                }
+
+
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun TestStatusBarHeight() {
+        val systemUiController = rememberSystemUiController()
+        SideEffect {
+            systemUiController.setStatusBarColor(Color.Transparent, darkIcons = true)
+        }
+        ProvideWindowInsets {
+            //ProvideWindowInsets中
+            //Modifier.statusBarsHeight()才生效
+            Surface(color = Color.Magenta) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Spacer(modifier = Modifier.statusBarsHeight())
+                    Text(text = "test04")
+                    Text(text = "test04")
+                }
 
             }
         }
@@ -50,9 +82,7 @@ class Main2Activity : ComponentActivity() {
     @Composable
     private fun TestRow() {
         Row(
-            modifier = Modifier
-                .padding(8.dp)
-                .background(Color.White),
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.Top
         ) {
             Text(text = stringResource(id = R.string.app_name))
